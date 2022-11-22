b4d9a45f0fadde46290a3a1a306885100f66ee52
Dries1234 <driesolbrechts@gmail.com>
2022-11-22 11:52:36 +0100

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace gitinder;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace gitinder;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
