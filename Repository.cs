5fe91ad77a9f485247e6387574a138d96dfff297
Dries1234 <driesolbrechts@gmail.com>
2022-12-05 12:02:08 +0100

    classes for repos

---
+++ Repository.cs
@@ -0,0 +1,24 @@
+﻿using IntelliJ.Lang.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitinder
+{
+    public class Repository
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int stargazerCount { get; set; }
+        public Language primaryLanguage { get; set; }
+
+        public string url { get; set; }
+    }
+    public class Language
+    {
+        public string name { get; set; }
+    }
+}
