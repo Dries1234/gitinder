0de89ce1757e6b4cde1a530cfd8fd7a6c47a7d67
Dries1234 <driesolbrechts@gmail.com>
2022-12-09 09:05:45 +0100

    Get repositories async from api and display in page

---
+++ ApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace gitinder
+{
+    internal class ApiController
+    {
+        public static async Task<Repository[]> GetPublicRepositories()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://localhost:49153/PublicRepo");
+
+            
+
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseString);
+            Repository[] repositories = JsonSerializer.Deserialize<Repository[]>(responseString);
+
+            return repositories;
+        }
+
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,15 +1,57 @@
-﻿namespace gitinder;
+﻿using System.Net.WebSockets;
+using System.Windows.Input;
 
-class ViewModel
+namespace gitinder;
+public class MainViewModel : ViewModelBase
 {
-	public Repository repo { get; set; }
+
+    private Repository _repo = null;
+    public int CurrentIndex = 0;
+    public Repository repo { get { return _repo; } set { _repo = value; OnPropertyChanged(); } }
+
+    public Repository[] repos { get; set; }
+
+    public ICommand RejectCommand { get; set; }
+    public ICommand AcceptCommand { get; set; }
+    public ICommand MatchesCommand { get; set; }
+
+    public MainViewModel()
+    {
+        RejectCommand = new Command(OnReject);
+        AcceptCommand = new Command(OnAccept);
+        MatchesCommand = new Command(OnMatches);
+    }
+
+    private void OnReject()
+    {
+        repo = repos[CurrentIndex];
+        CurrentIndex++;
+    }
+
+    private void OnAccept()
+    {
+        repo = repos[CurrentIndex];
+        CurrentIndex++;
+    }
+
+    private void OnMatches()
+    {
+        // Implement matches action here
+    }
 }
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        var vm = new MainViewModel();
+        BindingContext = vm;
+        // Implement reject action here
+        Task.Run(() => vm.repos = ApiController.GetPublicRepositories().Result).Wait();
+        vm.repo = vm.repos[vm.CurrentIndex];
+        vm.CurrentIndex++;
+
+    }
 }
 
